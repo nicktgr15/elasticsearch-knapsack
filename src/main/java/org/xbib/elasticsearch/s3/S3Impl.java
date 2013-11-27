0a7bfee7004665afa6d3d8571483f49208bda866
nicktgr15 <nicktgr15@gmail.com>
2013-11-27 15:12:18 +0000

    removed try/catch sections from S3Impl

--- src/main/java/org/xbib/elasticsearch/s3/S3Impl.java
+++ src/main/java/org/xbib/elasticsearch/s3/S3Impl.java
@@ -12,8 +12,6 @@ import org.elasticsearch.common.component.AbstractComponent;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.settings.Settings;
 
-import com.amazonaws.AmazonClientException;
-import com.amazonaws.AmazonServiceException;
 import com.amazonaws.auth.AWSCredentialsProviderChain;
 import com.amazonaws.auth.ClasspathPropertiesFileCredentialsProvider;
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
@@ -37,48 +35,20 @@ public class S3Impl extends AbstractComponent implements S3 {
     
     @Override
     public void writeToS3(String target, String s3Path) {
-        
         String bucketName = settings.get("s3.bucket_name");
         
-        try {
-            logger.debug("Uploading " + target + " to " + bucketName + " under " + s3Path);
-            File file = new File(target);
-            s3client.putObject(new PutObjectRequest(bucketName, s3Path, file));
-            file.delete();
-            
-        } catch (AmazonServiceException ase) {
-            logger.error("Caught an AmazonServiceException, which means your request made it "
-                    + "to Amazon S3, but was rejected with an error response.");
-            logger.error("Error Message: " + ase.getMessage());
-        } catch (AmazonClientException ace) {
-            logger.error("Caught an AmazonClientException, which means the client encountered "
-                    + "an internal error while trying to communicate with S3, "
-                    + "such as not being able to access the network.");
-           logger.error("Error Message: " + ace.getMessage());
-        }
-
+        logger.debug("Uploading " + target + " to " + bucketName + " under " + s3Path);
+        File file = new File(target);
+        s3client.putObject(new PutObjectRequest(bucketName, s3Path, file));
+        file.delete();
     }
 
     @Override
     public void readFromS3(String target, String s3Path) throws IOException {
-        
-        String bucketName = settings.get("s3.bucket_name");
+       String bucketName = settings.get("s3.bucket_name");
 
-        try {
-            S3Object object = s3client.getObject(new GetObjectRequest(bucketName, s3Path));
-            saveS3ObjectToDisk(object, target);
-            
-        } catch (AmazonServiceException ase) {
-            logger.error("Caught an AmazonServiceException, which means your request made it "
-                    + "to Amazon S3, but was rejected with an error response.");
-            logger.error("Error Message: " + ase.getMessage());
-        } catch (AmazonClientException ace) {
-            logger.error("Caught an AmazonClientException, which means the client encountered "
-                    + "an internal error while trying to communicate with S3, "
-                    + "such as not being able to access the network.");
-            logger.error("Error Message: " + ace.getMessage());
-        }
-
+       S3Object object = s3client.getObject(new GetObjectRequest(bucketName, s3Path));
+       saveS3ObjectToDisk(object, target);
     }
     
     private void saveS3ObjectToDisk(S3Object object, String filePath) throws IOException{
